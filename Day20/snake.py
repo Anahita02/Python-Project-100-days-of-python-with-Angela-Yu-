2b2af8cab2a2f436e55f956b3b41e41e0ed8b94d
Anahita02 <annataghadosi@gmail.com>
2025-08-17 17:28:46 +0330

    Day21

--- Day20/snake.py
+++ Day20/snake.py
@@ -15,12 +15,18 @@ class Snake:
 
     def creat_snake(self):
         for position in STARTING_POSITION:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        #Add a new segment to snake
+        self.add_segment(self.segments[-1].position())
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
